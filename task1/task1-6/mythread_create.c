9dcbf2c10a263d334823a72c07e6fd884007dfc9
I-Missha <I-Missha@github.com>
2024-11-08 15:21:27 +0700

    stack free

--- task1/task1-6/mythread_create.c
+++ task1/task1-6/mythread_create.c
@@ -1,122 +1,127 @@
 #define _GNU_SOURCE
-#include <stdlib.h>
+#include <err.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <sched.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <string.h>
-#include <stdio.h>
-#include <ucontext.h>
-#include <stdint.h>
 #include <linux/futex.h>
+#include <sched.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 #include <sys/syscall.h>
-#include <err.h>
+#include <threads.h>
+#include <ucontext.h>
+#include <unistd.h>
 
 #define PAGE_SIZE 4096
 #define STACK_SIZE PAGE_SIZE * 4
-
+#define MYTHREAD_CANCELLED (void *)1234453
 
 typedef unsigned long int mythread_t;
-typedef void* (*mythread_routine)(void*);
+typedef void *(*mythread_routine)(void *);
 
 typedef struct _mythread {
     int mythread_id;
     mythread_routine routine;
-    void* arg;
-    void* retval;
-    volatile int finished;
-    volatile int joined;
-    volatile uint32_t joinedFutex;
-    volatile uint32_t finishedFutex;
+    void *arg;
+    void *retval;
+    void *stack;
+    volatile int notFinished;
     volatile int isDetached;
-    ucontext_t ctx;
     volatile int isCancelled;
-}mythread_struct_t;
+    ucontext_t ctx;
+} mythread_struct_t;
 
-static uint32_t *futex1, *futex2, *iaddr;
-
-static int futex(uint32_t *uaddr, int futex_op, uint32_t val,
-                 const struct timespec *timeout, uint32_t *uaddr2, uint32_t val3) {
-    return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3);
-}
-
-static void fwait(uint32_t *futexp) {
-    long s;
-    s = futex(futexp, FUTEX_WAIT, 0, NULL, NULL, 0);
-    if (s == -1 && errno != EAGAIN)
-        err(EXIT_FAILURE, "futex FUTEX_WAIT");
-}
-
-static void fpost(uint32_t *futexp) {
-    long s;
-
-    s = futex(futexp, FUTEX_WAKE, 1, NULL, NULL, 0);
-    if (s  == -1)
-        err(EXIT_FAILURE, "futex FUTEX_WAKE");
-}
-
-void mythread_detach(mythread_struct_t* mythread) {
+mythread_struct_t *tid;
+void mythread_detach(mythread_struct_t *mythread) {
     mythread->isDetached = 1;
 }
 
-void mythread_testcancel(mythread_struct_t* thread) {
+void mythread_testcancel(mythread_struct_t *thread) {
     if (thread->isCancelled) {
         setcontext(&thread->ctx);
     }
 }
+thread_local ucontext_t new_context;
+thread_local mythread_struct_t local_thread;
+void free_stack_of_thread() {
+    munmap(
+        local_thread.stack + sizeof(mythread_struct_t) - STACK_SIZE, STACK_SIZE
+    );
+}
+int start_routine(void *arg) {
 
-int start_routine(void* arg) {
+    mythread_struct_t *thread = (mythread_struct_t *)arg;
 
-    mythread_struct_t* thread = (mythread_struct_t*)arg;
+    printf(
+        "mythread started doing a routine. thread_id: %d\n", thread->mythread_id
+    );
 
-    printf("mythread started doing a routine.\n thread_id: %d\n", thread->mythread_id);
     int err = getcontext(&thread->ctx);
+
     if (!thread->isCancelled) {
         thread->retval = thread->routine(thread->arg);
+    } else {
+        thread->retval = MYTHREAD_CANCELLED;
     }
-    thread->finished = 1;
-    fpost(&thread->finishedFutex);
-    //ToDo: make futex
-    printf("mythread started waiting stage for join.\n thread_id: %d\n", thread->mythread_id);
 
-    //if (!thread->joined && !thread->isDetached) {
-    //    fwait(&thread->joinedFutex);
-    //}
-    //while(!thread->joined && !thread->isDetached) {
-    //    sleep(1);
-    //}
-    printf("routine finished.\n thread_id: %d\n", thread->mythread_id);
+    if (thread->isDetached) {
+        void *new_stack = (void *)malloc(PAGE_SIZE);
+
+        new_context.uc_stack.ss_sp = new_stack;
+        new_context.uc_stack.ss_size = PAGE_SIZE;
+        new_context.uc_stack.ss_flags = 0;
+        new_context.uc_link = NULL;
+        local_thread.stack = thread->stack;
+        makecontext(&new_context, free_stack_of_thread, 0);
+        setcontext(&new_context);
+    }
+    printf("routine finished: thread_id: %d\n", thread->mythread_id);
     return 0;
 }
 
-
-void* create_stack(int size) {
-    void* stack;
-    stack = mmap(NULL, size, PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_STACK, -1, 0);
+void *create_stack(int size) {
+    void *stack;
+    stack = mmap(
+        NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0
+    );
     return stack;
 }
 
-
-int mythread_create(mythread_struct_t** mythread, mythread_routine routine, void* arg) {
+int mythread_create(
+    mythread_struct_t **mythread, mythread_routine routine, void *arg
+) {
     static int thread_num = 0;
-    mythread_struct_t *thread; 
+    mythread_struct_t *thread;
     thread_num++;
     int child_pid;
     void *child_stack;
 
     child_stack = create_stack(STACK_SIZE);
 
-    mprotect(child_stack + PAGE_SIZE, STACK_SIZE - PAGE_SIZE, PROT_READ | PROT_WRITE);
+    mprotect(
+        child_stack + PAGE_SIZE, STACK_SIZE - PAGE_SIZE, PROT_READ | PROT_WRITE
+    );
 
-    thread = (mythread_struct_t*)(child_stack + STACK_SIZE - sizeof(mythread_struct_t));
+    thread = (mythread_struct_t *)(child_stack + STACK_SIZE -
+                                   sizeof(mythread_struct_t));
+    thread->stack = child_stack;
     thread->mythread_id = thread_num;
     thread->arg = arg;
     thread->routine = routine;
     thread->notFinished = 1;
-    thread->joined = 0;
-    thread->isDetached = 1;
-    int err = clone(start_routine,  (void*)thread, CLONE_VM|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD|CLONE_CHILD_CLEARTID, thread);
+    thread->isDetached = 0;
+    int err = clone(
+        start_routine,
+        (void *)thread,
+        CLONE_VM | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD |
+            CLONE_CHILD_CLEARTID | CLONE_CHILD_CLEARTID,
+        thread,
+        &thread->notFinished,
+        NULL,
+        &thread->notFinished
+    );
     if (err == -1) {
         printf("clone failed: %s\n", strerror(errno));
         exit(-1);
@@ -125,38 +130,47 @@ int mythread_create(mythread_struct_t** mythread, mythread_routine routine, void
     return 0;
 }
 
-void mythread_cancel(mythread_struct_t* thread) {
-    thread->isCancelled = 1; 
+void mythread_cancel(mythread_struct_t *thread) {
+    thread->isCancelled = 1;
 }
-
-void* mythread_join(mythread_struct_t* mythread) {
-    printf("waiting for thread %d to be finished\n", mythread->mythread_id);
-    fwait(&mythread->finishedFutex);
-    //while(!mythread->finished) {
-    //    usleep(1);
-    //}
-
-    printf("mythread %d finished.\n", mythread->mythread_id);
-    printf("mythread return value is '%s'", (char*)mythread->retval);
-    mythread->joined = 1;
-    fpost(&mythread->joinedFutex);
-    return mythread->retval;
-}
-
-void* mythread(void* arg) {
-    for (int i = 0; i < 2; i++) {
-        printf("hello from mythread\n");
-        sleep(1);
+int mythread_join(mythread_struct_t *mythread, void **retval) {
+    printf("waiting for thread_id: %d to be finished\n", mythread->mythread_id);
+    if (mythread->isDetached) {
+        printf("%d is detached\n", mythread->mythread_id);
+        return -1;
     }
-    return "goodbye from my thread"; 
-}
+    while (mythread->notFinished) {
+        usleep(1);
+    }
 
+    if (retval != NULL) {
+        *retval = mythread->retval;
+    }
 
-int main() {
-    mythread_struct_t* tid;
-    mythread_create(&tid, mythread, "hello"); 
-    mythread_join(tid);
+    munmap(
+        mythread->stack + sizeof(mythread_struct_t) - STACK_SIZE, STACK_SIZE
+    );
     return 0;
 }
 
+void *mythread(void *arg) {
+    for (int i = 0; i < 5; i++) {
+        printf("hello from mythread\n");
+        sleep(1);
+        if (i == 2) {
+            mythread_testcancel(tid);
+        }
+    }
+    return "goodbye from my thread";
+}
 
+int main() {
+    mythread_create(&tid, mythread, "hello");
+    // mythread_detach(tid);
+    char retval[20];
+    sleep(1);
+    mythread_cancel(tid);
+    mythread_join(tid, (void *)retval);
+    sleep(7);
+    return 0;
+}
--- task1/task1-7/uthread.c
+++ task1/task1-7/uthread.c
@@ -1,62 +1,61 @@
 #define _GNU_SOURCE
-#include <stdlib.h>
+#include <assert.h>
+#include <err.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <sched.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <string.h>
-#include <stdio.h>
-#include <ucontext.h>
-#include <stdint.h>
 #include <linux/futex.h>
+#include <sched.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 #include <sys/syscall.h>
-#include <err.h>
-#include <assert.h>
+#include <ucontext.h>
+#include <unistd.h>
 
 #define PAGE_SIZE 4096
 #define STACK_SIZE PAGE_SIZE * 4
 #define NAME_SIZE 16
 #define MAX_THREADS 8
-#define MAX_STACKS_TO_CLEAR 100 
-
+#define MAX_STACKS_TO_CLEAR 100
 
 typedef struct uthread {
-    void* stack;
+    void *stack;
     char name[NAME_SIZE];
-    void (*thread_func)(void*);
-    void* arg;
+    void (*thread_func)(void *);
+    void *arg;
     ucontext_t uctx;
-    struct uthread* next;
-}uthread_t;
-
+    struct uthread *next;
+} uthread_t;
 
 typedef struct uthreads_list {
-    uthread_t* first;
-    uthread_t* last;
-    uthread_t* curr;
-}uthreads_list;
+    uthread_t *first;
+    uthread_t *last;
+    uthread_t *curr;
+} uthreads_list;
 
 typedef struct stack_to_clear {
-    void** stacksArr;
+    void **stacksArr;
     int currNum;
     int const timeToClear;
     int currTime;
-}stack_to_clear;
+} stack_to_clear;
 
-void* stacksArr[MAX_STACKS_TO_CLEAR]; 
+void *stacksArr[MAX_STACKS_TO_CLEAR];
 stack_to_clear stack_list = {stacksArr, 0, 20, 0};
 uthreads_list ulist = {NULL, NULL, NULL};
-uthread_t* curr_uthread_exe;
+uthread_t *curr_uthread_exe;
 
-//uthread_t *uthreads[MAX_THREADS];
-//int uthread_count = 0;
-//int uthread_curr = 0;
+// uthread_t *uthreads[MAX_THREADS];
+// int uthread_count = 0;
+// int uthread_curr = 0;
 
-
-void* create_stack(int size) {
-    void* stack;
-    stack = mmap(NULL, size, PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_STACK, -1, 0);
+void *create_stack(int size) {
+    void *stack;
+    stack = mmap(
+        NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0
+    );
     return stack;
 }
 
@@ -71,25 +70,25 @@ void uthread_scheduler() {
         stack_list.currTime = 0;
         stack_list.currNum = 0;
     }
-    //cur_ctx = &(uthreads[uthread_curr]->uctx);
-    //uthread_curr = (uthread_curr + 1) % uthread_count;
+    // cur_ctx = &(uthreads[uthread_curr]->uctx);
+    // uthread_curr = (uthread_curr + 1) % uthread_count;
     cur_ctx = &ulist.curr->uctx;
     next_ctx = &ulist.curr->next->uctx;
     ulist.curr = ulist.curr->next;
-    //next_ctx = &(uthreads[uthread_curr]->uctx);
+    // next_ctx = &(uthreads[uthread_curr]->uctx);
     err = swapcontext(cur_ctx, next_ctx);
     if (err == -1) {
         printf("error in uthread_schedulr: %s", strerror(errno));
         exit(1);
     }
-    //printf("%s\n", ulist.curr->name);    
+    // printf("%s\n", ulist.curr->name);
 }
 
 void start_thread() {
-    uthread_t* curr = ulist.curr;
+    uthread_t *curr = ulist.curr;
     curr->thread_func(curr->arg);
-    uthread_t* temp = ulist.first;
-    while(temp->next != curr) {
+    uthread_t *temp = ulist.first;
+    while (temp->next != curr) {
         temp = temp->next;
     }
     temp->next = curr->next;
@@ -106,7 +105,7 @@ void start_thread() {
         ctx = &uthreads[i]->uctx;
         char* stack_from = ctx->uc_stack.ss_sp;
         char* stack_to = ctx->uc_stack.ss_sp + ctx->uc_stack.ss_size;
-        
+
         char* temp = (char*)&i;
         if (stack_from <=  temp && temp <= stack_to) {
             uthreads[i]->thread_func(uthreads[i]->arg);
@@ -115,8 +114,10 @@ void start_thread() {
     */
 }
 
-void uthread_create(uthread_t **uthread, char *name, void (*thread_func)(void*), void* arg) {
-    char* stack;
+void uthread_create(
+    uthread_t **uthread, char *name, void (*thread_func)(void *), void *arg
+) {
+    char *stack;
     uthread_t *new_ut;
     int err;
 
@@ -125,11 +126,13 @@ void uthread_create(uthread_t **uthread, char *name, void (*thread_func)(void*),
     stack = create_stack(STACK_SIZE);
     mprotect(stack + PAGE_SIZE, STACK_SIZE - PAGE_SIZE, PROT_READ | PROT_WRITE);
 
-    new_ut = (uthread_t*)(stack + STACK_SIZE - sizeof(uthread_t));
-    
+    new_ut = (uthread_t *)(stack + STACK_SIZE - sizeof(uthread_t));
+
     err = getcontext(&new_ut->uctx);
     if (err == -1) {
-        printf("err in uthread_create getcontext failed: %s\n", strerror(errno));
+        printf(
+            "err in uthread_create getcontext failed: %s\n", strerror(errno)
+        );
         exit(2);
     }
     new_ut->stack = stack;
@@ -137,52 +140,51 @@ void uthread_create(uthread_t **uthread, char *name, void (*thread_func)(void*),
     new_ut->uctx.uc_stack.ss_size = STACK_SIZE - sizeof(uthread_t);
     new_ut->uctx.uc_link = NULL;
 
-
     makecontext(&new_ut->uctx, start_thread, 0);
-    
+
     new_ut->thread_func = thread_func;
     new_ut->arg = arg;
     strncpy(new_ut->name, name, NAME_SIZE);
     new_ut->next = ulist.first;
     ulist.last->next = new_ut;
     ulist.last = new_ut;
-    //uthreads[uthread_count] = new_ut;
-    //uthread_count++;
+    // uthreads[uthread_count] = new_ut;
+    // uthread_count++;
     *uthread = new_ut;
 }
 
-void uthread(void* arg) {
+void uthread(void *arg) {
     for (int i = 0; i < 2; i++) {
-        printf("hello from: %s\n", (char*)arg);
+        printf("hello from: %s\n", (char *)arg);
         sleep(1);
         uthread_scheduler();
     }
     return;
-} 
+}
 int ones = 0;
 int main() {
 
     uthread_t main_thread;
-    //uthreads[0] = &main_thread;
-    //uthread_count++;
+    // uthreads[0] = &main_thread;
+    // uthread_count++;
     ulist.curr = &main_thread;
     ulist.first = &main_thread;
     ulist.last = &main_thread;
-    uthread_t* ut0;
-    uthread_t* ut1;
+    uthread_t *ut0;
+    uthread_t *ut1;
     strncpy(main_thread.name, "user thread main", NAME_SIZE);
     main_thread.next = NULL;
     getcontext(&main_thread.uctx);
-    if (!ones) {
-        uthread_create(&ut0, "user thread zero", uthread, "000 000");
-        uthread_create(&ut1, "user thread one", uthread, "111 111");
-        uthread_create(&ut1, "user thread two", uthread, "222 222");
-        ones = 1;
-    }
+    // if (!ones) {
+    uthread_create(&ut0, "user thread zero", uthread, "000 000");
+    uthread_create(&ut1, "user thread one", uthread, "111 111");
+    uthread_create(&ut1, "user thread two", uthread, "222 222");
+    ones = 1;
+    //}
     int a = 0;
     while (a < 100) {
         uthread_scheduler();
-    //    printf("main\n");
+        //    printf("main\n");
         a++;
     }
     printf("main\n");
